0b87af5210ef773963c8bd68ac8621cb1aa02d85
The-solo <utkarshsase4321@gmail.com>
2026-07-11 20:25:00 +0530

    Fixed mistake & understood the array casting

--- basics/arrays/arrayCasting.c
+++ basics/arrays/arrayCasting.c
@@ -6,12 +6,15 @@ typedef struct Graphics {
   int width;
 } graphics_t;
 
+
 void dump_graphics(graphics_t gsettings[10]) {
-    int *ptr = (int *)gsettings; // casting the array of int to array of structs. 
-    for (int i = 0; i < 10; i++) {
+    int *ptr = (int *)gsettings; // casting the array struct to an array of int.(Not recommended)
+    for (int i = 0; i < 10 * 3; i++) { // each struct has 3 elements.
         printf("settings[%d] = %d\n", i, ptr[i]);
     }
 }
+
+// By casting structs to int C knows how to access them in memory. one after another.
 
 int main() {
     return 0;
