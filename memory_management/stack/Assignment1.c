e5b3f2542a148c615bab13fcc88f6066eefef053
The-solo <utkarshsase4321@gmail.com>
2026-07-22 16:48:57 +0530

    Intro to memory stack

---
+++ memory_management/stack/Assignment1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+// we should always declare the function on top so the compiler knows.
+void printMessageOne(void);
+void printMessageTwo(void);
+void printMessageThree(void);
+void printStackPointerDiff(void);
+
+int main() {
+
+    // These functions are called one after another to multiple stack allocations.
+    printMessageOne();
+    printMessageTwo();
+    printMessageThree();
+    return 0;
+}
+
+// __attribute__((noinline)) helps the compiler behave; don't worry about it
+__attribute__((noinline)) void printMessageOne(void) {
+    const char *message = "Dark mode?\n";
+    printStackPointerDiff();
+    printf("%s\n", message);
+}
+
+__attribute__((noinline)) void printMessageTwo(void) {
+    const char *message = "More like...\n";
+    printStackPointerDiff();
+    printf("%s\n", message);
+}
+
+__attribute__((noinline)) void printMessageThree(void) {
+    const char *message = "dark roast.\n";
+    printStackPointerDiff();
+    printf("%s\n", message);
+}
+
+// don't touch below this line
+
+void printStackPointerDiff(void) {
+    static void *last_sp = NULL;
+    void *current_sp;
+    current_sp = __builtin_frame_address(0);
+    long diff;
+    if (last_sp == NULL) {
+        last_sp = current_sp;
+        diff = 0;
+    } else {
+        diff = (char *)last_sp - (char *)current_sp;
+    }
+    printf("---------------------------------\n");
+    printf("Stack pointer offset: %ld bytes\n", diff);
+    printf("---------------------------------\n");
+}
